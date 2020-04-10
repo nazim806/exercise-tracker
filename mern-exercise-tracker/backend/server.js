f2f96d1a9193367972f0a178b3579117dc3270b5
Nazim Uddin <umnazim@gmail.com>
2020-04-09 23:55:27 -0700

    routers created

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -11,11 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
-connection.once('open', () =>{
-    console.log("MongoDB database connection established successfully");
-})
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./route/users");
+
+app.use("./exercises", exercisesRouter);
+app.use("./users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
