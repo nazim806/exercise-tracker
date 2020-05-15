d444d8bf29197b89ebe4ab48d8d1a2d4f71b19c5
Nazim Uddin <umnazim@gmail.com>
2020-05-15 14:05:33 -0700

    create component and add them to app

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
-import ExerciseList from "./components/exercise-list.component";
+import ExerciseList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
@@ -11,12 +11,14 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
-      <Navbar />
-      <br />
-      <Route path="/" exact component={ExerciseList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExerciseList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
---
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class ExerciseList extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the exercise list component.</p>
+      </div>
+    );
+  }
+}
