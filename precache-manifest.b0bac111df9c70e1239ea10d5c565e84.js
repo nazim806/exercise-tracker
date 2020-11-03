478136f5e4098004edb637090d81ab8608d561a9
Nazim Uddin <umnazim@gmail.com>
2020-11-02 19:19:43 -0800

    Updates

---
+++ precache-manifest.b0bac111df9c70e1239ea10d5c565e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f17ec37433206c808c265c7b8c8351",
+    "url": "/exercise-tracker/index.html"
+  },
+  {
+    "revision": "750629c04377869d5b62",
+    "url": "/exercise-tracker/static/css/2.94ad3b50.chunk.css"
+  },
+  {
+    "revision": "750629c04377869d5b62",
+    "url": "/exercise-tracker/static/js/2.6453a0b2.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercise-tracker/static/js/2.6453a0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68304a84ff64b811d64",
+    "url": "/exercise-tracker/static/js/main.f9469661.chunk.js"
+  },
+  {
+    "revision": "d29f1920f7bbfeb8341e",
+    "url": "/exercise-tracker/static/js/runtime-main.c46a11e0.js"
+  }
+]);
